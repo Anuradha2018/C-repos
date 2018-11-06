7cf007651f8fef386477ec167b45197bbfdc9911
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2018-11-06 22:19:49 +0100

    Lab 1 of my learning

---
+++ Program - Copy.cs
@@ -0,0 +1,68 @@
+﻿//point 4 :- Import the namespace system
+using System;
+// Point 1 :- Namespace --> Class --> Method
+namespace Lab1
+{
+    //class Program
+    //{
+    //    // Point 2 :- Main means this is the first 
+    //    // thing that will execute
+    //    static void Main(string[] args) // Point 3 :- In args we have parameters
+    //    {
+    //        Console.WriteLine("Enter how many record ?");
+    //        int numrecord = Convert.ToInt16(Console.ReadLine());
+    //        for (int i=0;i<numrecord;i++)
+    //        {
+    //            try
+    //            {
+    //                string Name = "";
+    //                int Age = 0;
+    //                Console.WriteLine("Enter Name");
+    //                Name = Console.ReadLine(); // Point 5 read lines takes data from key
+    //                if (Name.Length == 0)
+    //                {
+    //                    Console.WriteLine("Name is required");
+    //                    Console.Read();
+    //                    return;
+    //                }
+    //                Console.WriteLine("Enter Age");
+    //                Age = Convert.ToInt16(Console.ReadLine());
+    //                if (Age > 100)
+    //                {
+    //                    Console.WriteLine("Age is not proper");
+    //                    Console.Read();
+    //                    return;
+    //                }
+    //                Console.WriteLine("Hello " + Name + "  " + Age);
+
+    //            }
+    //            catch (Exception e)
+    //            {
+    //                Console.WriteLine("Please try ..invalid data");
+    //            }
+    //        }
+    //        Console.ReadLine();
+           
+    //    }
+    //}
+
+    //class Person
+    //{
+    //    public string Name { get; set; }
+    //    public int Age { get; set; }
+    //    public string Address { get; set; }
+    //    public decimal Salary { get; set; }
+    //    public bool Valid()
+    //    {
+    //        if(Name.Length==0)
+    //        {
+    //            return false; // below codes do not execute
+    //        }
+    //        if(Age > 100)
+    //        {
+    //            return false;
+    //        }
+    //        return true;
+    //    }
+    //}
+}
